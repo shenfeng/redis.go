78f56801c797a51b9b91f4560a392f5839c6659c
feng <shenedu@gmail.com>
2013-07-08 19:58:20 +0800

    small change

--- redis.go
+++ redis.go
@@ -1,26 +1,31 @@
 package redis
 
 import (
-	"net"
+	"encoding/json"
 	"errors"
-	"strconv"
 	"fmt"
-	//	"log"
-	"log"
+	// "log"
+	"net"
+	"reflect"
+	"strconv"
 )
 
 const (
 	defaultPoolSize = 5
-	bufferSize      = 1024*24
+	bufferSize      = 1024 * 24
 )
 
+type RedisError string
+
+func (err RedisError) Error() string { return "Redis Error: " + string(err) }
+
 type ByteBuffer struct {
-	buffer                                                                                        []byte
-	pos,                                                                                    limit int
+	buffer     []byte
+	pos, limit int
 }
 
 type RedisConn struct {
-	conn    net.Conn
+	conn net.Conn
 	rbuf *ByteBuffer
 	wbuf *ByteBuffer
 }
@@ -32,7 +37,7 @@ func (p *ByteBuffer) writeInt(i int) {
 	} else {
 		pos := p.pos
 		start := pos
-		for ; i > 0; i = i/10 {
+		for ; i > 0; i = i / 10 {
 			p.buffer[pos] = byte(i%10 + '0')
 			pos += 1
 		}
@@ -47,9 +52,19 @@ func (p *ByteBuffer) writeInt(i int) {
 	}
 }
 
+func toBytes(value interface{}) []byte {
+	switch v := value.(type) {
+	case string:
+		return []byte(v)
+	case []byte:
+		return v
+	}
+	panic("Only []byte, string is understandable")
+}
+
 func (p *ByteBuffer) writeCRLF() {
 	p.buffer[p.pos] = '\r'
-	p.buffer[p.pos + 1] = '\n'
+	p.buffer[p.pos+1] = '\n'
 	p.pos += 2
 }
 
@@ -62,7 +77,16 @@ func (p *ByteBuffer) writeString(s string) {
 	p.pos += len(s)
 }
 
-func (buf *ByteBuffer) encodeRequest(cmd string, args []string) {
+func (p *ByteBuffer) writeBytes(bs []byte) {
+	p.buffer[p.pos] = '$'
+	p.pos += 1
+	p.writeInt(len(bs))
+	p.writeCRLF()
+	copy(p.buffer[p.pos:], bs)
+	p.pos += len(bs)
+}
+
+func (buf *ByteBuffer) encodeRequest(cmd string, args [][]byte) {
 	buf.buffer[0] = '*'
 	buf.pos = 1
 	buf.writeInt(len(args) + 1)
@@ -72,10 +96,10 @@ func (buf *ByteBuffer) encodeRequest(cmd string, args []string) {
 	buf.writeCRLF()
 
 	for _, s := range args {
-		if buf.pos + len(s) + 20 > cap(buf.buffer) {
-			buf.buffer = make([]byte, cap(buf.buffer)*2 + len(s))
+		if buf.pos+len(s)+20 > cap(buf.buffer) {
+			buf.buffer = make([]byte, cap(buf.buffer)*2+len(s))
 		}
-		buf.writeString(s)
+		buf.writeBytes(s)
 		buf.writeCRLF()
 	}
 }
@@ -95,7 +119,7 @@ func (client *Client) getCon() (*RedisConn, error) {
 }
 
 func (c *RedisConn) readMore() error {
-	if c.rbuf.limit + 10 > cap(c.rbuf.buffer) {
+	if c.rbuf.limit+10 > cap(c.rbuf.buffer) {
 		copy(c.rbuf.buffer, c.rbuf.buffer[c.rbuf.pos:c.rbuf.limit])
 		pos := c.rbuf.pos
 		c.rbuf.pos = 0
@@ -124,21 +148,71 @@ func (c *RedisConn) readLine() ([]byte, error) {
 		}
 		c.rbuf.pos += 1
 	}
-	return c.rbuf.buffer[start:c.rbuf.pos - 2], nil
+	return c.rbuf.buffer[start : c.rbuf.pos-2], nil
 }
 
-func (client *Client) sendCommand(cmd string, args ...string) (interface {}, error) {
-	c, err := client.getCon()
-	defer func() {
-		client.returnCon(c)
-	}()
-
+func (c *RedisConn) readResponse() (interface{}, error) {
+	if c.rbuf.pos == c.rbuf.limit {
+		err := c.readMore()
+		if err != nil {
+			return nil, err
+		}
+	}
+	pos := c.rbuf.pos
+	c.rbuf.pos += 1
+	line, err := c.readLine()
 	if err != nil {
 		return nil, err
 	}
+	switch c.rbuf.buffer[pos] {
+	case '+':
+		return line, nil
+	case '-':
+		return nil, errors.New(string(line))
+	case ':':
+		return strconv.Atoi(string(line))
+	case '$':
+		length, err := strconv.Atoi(string(line))
+		if length > 0 {
+			if err != nil {
+				return nil, err
+			}
+			if c.rbuf.pos+length+2 > cap(c.rbuf.buffer) {
+				tmp := make([]byte, c.rbuf.pos+length+2)
+				copy(tmp, c.rbuf.buffer[:c.rbuf.limit])
+				c.rbuf.buffer = tmp
+			}
+			for c.rbuf.pos+length+2 > c.rbuf.limit {
+				err := c.readMore()
+				if err != nil {
+					return nil, err
+				}
+			}
+			start := c.rbuf.pos
+			c.rbuf.pos += length + 2
+			return c.rbuf.buffer[start : c.rbuf.pos-2], nil
+		} else {
+			return nil, errors.New("No such key")
+		}
+	case '*':
+		size, err := strconv.Atoi(string(line))
+		if err != nil {
+			return nil, RedisError("MultiBulk reply expected a number")
+		}
+		rets := make([]interface{}, size)
+		for i := 0; i < size; i++ {
+			rets[i], err = c.readResponse()
+			if err != nil {
+				return nil, err
+			}
+		}
+		return rets, nil
+	}
+	return nil, fmt.Errorf("Unkown %s", c.rbuf.buffer[0:1])
+}
 
+func (c *RedisConn) sendCommand(cmd string, args ...[]byte) (interface{}, error) {
 	c.wbuf.encodeRequest(cmd, args)
-
 	pos := 0
 	for pos < c.wbuf.pos {
 		n, err := c.conn.Write(c.wbuf.buffer[pos:c.wbuf.pos])
@@ -147,55 +221,8 @@ func (client *Client) sendCommand(cmd string, args ...string) (interface {}, err
 		}
 		pos += n
 	}
-
-	n, err := c.conn.Read(c.rbuf.buffer)
-
-	if err != nil {
-		return nil, err
-	}
-	c.rbuf.pos, c.rbuf.limit = 1, n
-	line, err := c.readLine()
-	if err != nil {
-		return nil, err
-	}
-	log.Println("exec----------", cmd, args)
-	switch c.rbuf.buffer[0] {
-	case '+':
-		return line, nil
-	case '-':
-		return nil, errors.New(string(line))
-	case ':':
-		return strconv.Atoi(string(line))
-	case '$':
-
-		length, err := strconv.Atoi(string(line))
-		if length > 0 {
-			if err != nil {
-				return nil, err
-			}
-
-			if c.rbuf.pos + length + 2 < cap(c.rbuf.buffer) {
-				tmp := make([]byte, c.rbuf.pos + length + 2)
-				copy(tmp, c.rbuf.buffer[:c.rbuf.limit])
-				c.rbuf.buffer = tmp
-			}
-
-			for c.rbuf.pos + length + 2 < c.rbuf.limit {
-				n, err := c.conn.Read(c.rbuf.buffer[c.rbuf.limit:])
-				if err != nil {
-					return nil, err
-				}
-				c.rbuf.limit += n
-			}
-			log.Println("---$---", c.rbuf.pos, c.rbuf.limit, length)
-			return c.rbuf.buffer[c.rbuf.pos:c.rbuf.limit - 2], nil
-		} else {
-			return nil, fmt.Errorf("No key %s", args[0])
-		}
-
-	}
-
-	return nil, nil
+	c.rbuf.pos, c.rbuf.limit = 0, 0
+	return c.readResponse()
 }
 
 func (client *Client) returnCon(c *RedisConn) {
@@ -227,31 +254,139 @@ func NewClient(addr string, db int) (*Client, error) {
 	}
 }
 
-func (c *Client) Get(key string) (string, error) {
-	value, err := c.sendCommand("GET", key)
+func mappingToArgs(v reflect.Value, args *[][]byte) error {
+	switch v.Kind() {
+	case reflect.Ptr:
+		return mappingToArgs(reflect.Indirect(v), args)
+	case reflect.Interface:
+		return mappingToArgs(v.Elem(), args)
+	case reflect.Map:
+		if v.Type().Key().Kind() != reflect.String {
+			return errors.New("Unsupported type - map key must be a string")
+		}
+		keys := v.MapKeys()
+		for _, k := range keys {
+			*args = append(*args, []byte(k.String()))
+			bs, err := json.Marshal(v.MapIndex(k).Interface())
+			if err != nil {
+				return err
+			}
+			*args = append(*args, bs)
+		}
+		return nil
+	}
+	panic("v")
+}
+
+func (client *Client) Hmset(key string, mapping map[string]interface{}) error {
+	c, err := client.getCon()
+	defer func() {
+		client.returnCon(c)
+	}()
+
+	var args [][]byte
+	args = append(args, []byte(key))
+	// err := mappingToArgs(reflect.ValueOf(mapping), &args)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = c.sendCommand("HMSET", args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *Client) Hgetall(key string) (m map[string]string, err error) {
+	c, err := client.getCon()
+	defer func() {
+		client.returnCon(c)
+	}()
+
+	rets, err := c.sendCommand("HGETALL", []byte(key))
+	if err != nil {
+		return m, err
+	}
+
+	for i := 0; i < len(rets.([]interface{})); i += 2 {
+		// m[rets
+	}
+	return m, err
+}
+
+func (client *Client) Setnx(key string, data interface{}) (bool, error) {
+	c, err := client.getCon()
+	defer func() {
+		client.returnCon(c)
+	}()
+
+	v, err := c.sendCommand("SETNX", []byte(key), toBytes(data))
+	if err != nil {
+		return false, err
+	}
+	return v.(int) == 1, nil
+}
+
+func (client *Client) Setex(key string, seconds int, data interface{}) error {
+	c, err := client.getCon()
+	_, err = c.sendCommand("SET", []byte(key), []byte(strconv.Itoa(seconds)), toBytes(data))
+	client.returnCon(c)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *Client) Set(key string, data interface{}) error {
+	c, err := client.getCon()
+	_, err = c.sendCommand("SET", []byte(key), toBytes(data))
+	client.returnCon(c)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *Client) Get(key string) ([]byte, error) {
+	c, err := client.getCon()
+	defer func() {
+		client.returnCon(c)
+	}()
+
+	value, err := c.sendCommand("GET", []byte(key))
+	if err != nil {
+		return nil, err
+	}
+	val := value.([]byte)
+	copied := make([]byte, len(val))
+	// value is just a reference to a buffer, need copy to protect buffer
+	copy(copied, val)
+	return val, err
+}
+
+func (client *Client) GetString(key string) (string, error) {
+	c, err := client.getCon()
+	defer func() {
+		client.returnCon(c)
+	}()
+
+	value, err := c.sendCommand("GET", []byte(key))
 	if err != nil {
 		return "", err
 	}
 	return string(value.([]byte)), err
 }
 
-func (c *Client) Set(key, value string) error {
-	v, err := c.sendCommand("GET", key)
+func (client *Client) Del(key string) error {
+	c, err := client.getCon()
+	_, err = c.sendCommand("Del", []byte(key))
+	client.returnCon(c)
 	if err != nil {
 		return err
 	}
-	log.Println("----------set----------", string(v.([]byte)))
 	return nil
 }
-
-func (c *Client) Del(key string) error {
-	v, err := c.sendCommand("Del", key)
-	if err != nil {
-		return err
-	}
-	log.Println("----------del----------", v)
-	return nil
-}
-
-
-
--- redis_test.go
+++ redis_test.go
@@ -1,12 +1,20 @@
 package redis
 
 import (
-	"testing"
+	"fmt"
 	"log"
 	"strconv"
+	"testing"
+)
+
+const (
+	myKey   = "mykey"
+	myValue = "myValue"
 )
 
 var client *Client
+
+var testObj map[string]interface{} = map[string]interface{}{"key1": "value1", "key2": "value2", "key3": 101}
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -30,54 +38,100 @@ func TestWriteInt(t *testing.T) {
 
 func TestEncodingRequest(t *testing.T) {
 	buf := &ByteBuffer{buffer: make([]byte, 1024)}
-	buf.encodeRequest("SET", []string{"mykey", "myvalue"})
+	buf.encodeRequest("SET", [][]byte{[]byte(myKey), []byte(myValue)})
 	encoded := string(buf.buffer[:buf.pos])
-	expect := "*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n"
+
+	expect := fmt.Sprintf("*3\r\n$3\r\nSET\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",
+		len(myKey), myKey, len(myValue), myValue)
 	if encoded != expect {
 		t.Errorf("get %s, but should be %s\n", encoded, expect)
 	}
 }
 
-func TestGet(t *testing.T) {
-	client.Set("myKey", "myValue")
-	for i := 0; i < 10; i ++ {
-		v, _ := client.Get("myKey")
-		if v != "myValue" {
-			t.Errorf("get %s, but shoule be %s\n", v, "myValue")
+func TestSet(t *testing.T) {
+
+	client.Set(myKey, myValue)
+	for i := 0; i < 4; i++ {
+		v, _ := client.GetString(myKey)
+		if v != myValue {
+			t.Errorf("get %s, but shoule be %s\n", v, myValue)
 		}
 	}
-	client.Del("myKey")
+	client.Del(myKey)
+
+	client.Set(myKey, []byte(myValue))
+	v, _ := client.Get(myKey)
+	if string(v) != myValue {
+		t.Errorf("get %s, but shoule be %s\n", v, myValue)
+	}
+
+	for i := 0; i < 2; i++ {
+		client.Del(myKey)
+		r, _ := client.Setnx(myKey, myValue)
+		if !r {
+			t.Error("setnx should ok the first time")
+		}
+		r, _ = client.Setnx(myKey, myValue)
+		if r {
+			t.Error("setnx should fail the second time")
+		}
+	}
+
+	client.Del(myKey)
+	client.Setex(myKey, []byte(myValue))
+}
+
+func TestHmset(t *testing.T) {
+	client.Del(myKey)
+	client.Hmset(myKey, testObj)
+	client.Hgetall(myKey)
 }
 
 func BenchmarkEncodingRequest(b *testing.B) {
 	buf := &ByteBuffer{buffer: make([]byte, 1024)}
-	buf.encodeRequest("SET", []string{"mykey", "myvalue"})
+	tmp := [][]byte{[]byte(myKey), []byte(myValue)}
+	buf.encodeRequest("SET", tmp)
 	b.SetBytes(int64(buf.pos))
 
 	for i := 0; i < b.N; i++ {
-		buf.encodeRequest("SET", []string{"mykey", "myvalue"})
+		buf.encodeRequest("SET", tmp)
 	}
 }
 
 func BenchmarkGetConn(b *testing.B) {
-	for i := 0; i < b.N; i ++ {
+	for i := 0; i < b.N; i++ {
 		c, _ := client.getCon()
 		client.returnCon(c)
 	}
 }
 
 func BenchmarkGet(b *testing.B) {
-	client.Set("myKey", "myValue")
-	for i := 0; i < b.N; i ++ {
-		client.Get("myKey")
+	client.Set(myKey, myValue)
+	for i := 0; i < b.N; i++ {
+		client.Get(myKey)
 	}
-	client.Del("myKey")
+	client.Del(myKey)
 }
 
-func BenchmarkStringCopy(b *testing.B) {
-	s := "GET"
-	buffer := make([]byte, 1024)
+func BenchmarkGetString(b *testing.B) {
+	client.Set(myKey, myValue)
 	for i := 0; i < b.N; i++ {
-		copy(buffer[:], []byte(s))
+		client.GetString(myKey)
 	}
+	client.Del(myKey)
+}
+
+func BenchmarkSet(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		client.Set(myKey, myValue)
+	}
+	client.Del(myKey)
+}
+
+func BenchmarkHmset(b *testing.B) {
+	client.Del(myKey)
+	for i := 0; i < b.N; i++ {
+		client.Hmset(myKey, testObj)
+	}
+	client.Del(myKey)
 }
